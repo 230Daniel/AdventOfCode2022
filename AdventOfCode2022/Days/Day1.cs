f0375a4456c12ab369be9349b341b02f87659d0d
Daniel Baynton <daniel.baynton@hotmail.com>
2022-12-01 07:49:39 +0000

    Day1

---
+++ AdventOfCode2022/Days/Day1.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode2022.Days;
+
+public class Day1 : Day
+{
+    public Day1() : base(1) { }
+
+    protected override object PartOne(string[] input)
+    {
+        var mostCalories = 0;
+        var currentCalories = 0;
+        foreach (var line in input)
+        {
+            if (line == "")
+            {
+                if (currentCalories > mostCalories) mostCalories = currentCalories;
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += int.Parse(line);
+            }
+        }
+
+        return mostCalories;
+    }
+
+    protected override object PartTwo(string[] input)
+    {
+        var mostCalories = new List<int> {0, 0, 0};
+        var currentCalories = 0;
+
+        foreach (var line in input)
+        {
+            if (line == "")
+            {
+                var minMostCalories = mostCalories.Min();
+                if (currentCalories > minMostCalories)
+                {
+                    mostCalories.Remove(minMostCalories);
+                    mostCalories.Add(currentCalories);
+                }
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += int.Parse(line);
+            }
+        }
+
+        return mostCalories.Sum();
+    }
+}
